b6a3e858f58a31b98dc4d0debd2c92cd0a552605
Zachary W Freeman <z8applepi@gmail.com>
2025-12-03 11:21:39 -0600

    Extend SingleParticleHamiltonian with energy initialization and base coupling attribute.

--- hamiltonian_construction/hamiltonians.py
+++ hamiltonian_construction/hamiltonians.py
@@ -8,20 +8,38 @@ class BaseHamiltonian:
         pass
 
     def diagonalize(self, *args):
-        raise NotImplementedError("Need to implement diagonalize method for Hamiltonian.")
+        raise NotImplementedError("Need to implement diagonalize method for "
+                                  "Hamiltonian.")
 
     def visualize_couplings(self, *args):
-        raise NotImplementedError("Need to implement visualize_couplings method for Hamiltonian.")
+        raise NotImplementedError("Need to implement visualize_couplings method for "
+                                  "Hamiltonian.")
 
 
 class SingleParticleHamiltonian(BaseHamiltonian):
     """
     Single particle Hamiltonian
     """
-    def __init__(self, len, dim):
+    def __init__(self, nlen, ndim, energy=0., base_coupling=1., list_energies=None):
         super().__init__()
-        self.len = len
-        self.dim = dim
+        self.len = nlen
+        self.dim = ndim
 
         # Generate state list for indexing diagonal of Hamiltonian.
-        self.state_list = list(itertools.product([x for x in range(len)], repeat=dim))
+        self.state_list = list(itertools.product([x for x in range(nlen)], repeat=ndim))
+
+        # Generate diagonal energies
+        if list_energies is not None:
+            if nlen(list_energies) != nlen(self.state_list):
+                raise ValueError(
+                    f"Length of list_energies ({nlen(list_energies)}) does not match "
+                    f"length of state_list ({nlen(self.state_list)})"
+                )
+            self.energies = list_energies
+        else:
+            self.energies = [energy for _ in range(nlen(self.state_list))]
+
+        self.base_coupling = base_coupling
+
+    def construct_hamiltonian(self):
+        raise NotImplementedError("Need to implement construct_hamiltonian method")
