d19a17abadd63d588bd34a9b5268162d2e67d550
Zachary W Freeman <z8applepi@gmail.com>
2025-12-03 12:29:16 -0600

    Refactor BaseHamiltonian and SingleParticleHamiltonian constructors to improve attribute initialization and reuse lattice basis generation.

--- hamiltonian_construction/hamiltonians.py
+++ hamiltonian_construction/hamiltonians.py
@@ -6,8 +6,22 @@ class BaseHamiltonian:
     """
     Base Hamiltonian class.
     """
-    def __init__(self):
-        pass
+    def __init__(self, nlen, ndim, base_coupling=1., coupling_type='nearest neighbor',
+                 minimum_coupling=0.):
+        self.len = nlen
+        self.dim = ndim
+
+        # Generate lattice basis
+        self.lattice_basis = list(itertools.product([x for x in range(nlen)], repeat=ndim))
+
+        # Add coupling information
+        self.base_coupling = base_coupling
+        if coupling_type == 'nearest neighbor' or coupling_type == 'dipole':
+            self.coupling_type = coupling_type
+        else:
+            raise ValueError(f"Coupling_type {coupling_type} not recognized. Accepted "
+                             f"values are 'nearest neighbor' and 'dipole'.")
+        self.minimum_coupling = minimum_coupling
 
     def diagonalize(self, *args):
         raise NotImplementedError("Need to implement diagonalize method")
@@ -23,14 +37,12 @@ class SingleParticleHamiltonian(BaseHamiltonian):
     """
     Single particle Hamiltonian
     """
-    def __init__(self, nlen, ndim, energy=0., base_coupling=1., list_energies=None,
+    def __init__(self, nlen, ndim, energy=0., list_energies=None, base_coupling=1.,
                  coupling_type='nearest neighbor', minimum_coupling=0.):
-        super().__init__()
-        self.len = nlen
-        self.dim = ndim
+        super().__init__(nlen, ndim, base_coupling, coupling_type, minimum_coupling)
 
         # Generate state list for indexing diagonal of Hamiltonian.
-        self.state_list = list(itertools.product([x for x in range(nlen)], repeat=ndim))
+        self.state_list = self.lattice_basis
 
         # Generate diagonal energies
         if list_energies is not None:
@@ -42,14 +54,6 @@ class SingleParticleHamiltonian(BaseHamiltonian):
             self.energies = list_energies
         else:
             self.energies = [energy for _ in range(nlen(self.state_list))]
-
-        # Add coupling information
-        self.base_coupling = base_coupling
-        if coupling_type == 'nearest neighbor' or coupling_type == 'dipole':
-            self.coupling_type = coupling_type
-        else:
-            raise ValueError(f"Coupling_type {coupling_type} not recognized. Accepted "
-                             f"values are 'nearest neighbor' and 'dipole'.")
 
     def construct_hamiltonian(self):
         raise NotImplementedError("Need to implement construct_hamiltonian method")
