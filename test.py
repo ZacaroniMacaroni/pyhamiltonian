d7bb0adb2bda0f4d7a31b527a50f9fb178dbb6fa
Zachary W Freeman <z8applepi@gmail.com>
2025-12-05 16:02:40 -0600

    Initialize `state_list` and `energies` placeholders in `BaseHamiltonian`.

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from hamiltonian_construction.hamiltonians import SingleParticleHamiltonian, \
     ManyBodyHamiltonian
 
-ham = SingleParticleHamiltonian(3, 2)
+ham = SingleParticleHamiltonian(2, 2)
 print(ham.state_list)
+print(ham.construct_hamiltonian())
 
-mbham = ManyBodyHamiltonian(3, 2, 2)
+mbham = ManyBodyHamiltonian(2, 2, 2)
 print(mbham.state_list)
+print(mbham.construct_hamiltonian())
