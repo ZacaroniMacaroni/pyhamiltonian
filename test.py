716192914584e1a7fd96dd1260cc59251d47cf16
Zachary W Freeman <z8applepi@gmail.com>
2025-12-07 15:33:04 -0600

    Fix minor naming inconsistency in file `n_dimensional_scripts.py`.

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from hamiltonian_construction.hamiltonians import SingleParticleHamiltonian, \
-    ManyBodyHamiltonian
+    ManyBodyHamiltonian, Lattice
+from plotting.n_dimensional_scripts import NDimPlot
 
-ham = SingleParticleHamiltonian(2, 2)
+lat = Lattice(2, 2)
+
+ham = SingleParticleHamiltonian(lat, 1.)
 print(ham.state_list)
 print(ham.construct_hamiltonian())
 
-mbham = ManyBodyHamiltonian(2, 2, 2)
+nd_plot = NDimPlot(ham)
+fig, ax = nd_plot.plot_energy_spectrum()
+fig.show()
+
+mbham = ManyBodyHamiltonian(lat, 2, 1.)
 print(mbham.state_list)
 print(mbham.construct_hamiltonian())
