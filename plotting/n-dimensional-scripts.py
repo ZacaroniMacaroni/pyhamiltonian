06979d4d6ba6c8ec51947203ba4198919ccb984f
Zachary W Freeman <z8applepi@gmail.com>
2025-12-07 15:24:47 -0600

    Add `plot_eigenstate_population` method and `_project_eigvec` placeholder method to `NDimPlot`.

--- plotting/n-dimensional-scripts.py
+++ plotting/n-dimensional-scripts.py
@@ -28,3 +28,52 @@ class NDimPlot:
         ax.plot(eigvals)
         return fig, ax
 
+    def _project_eigvec(self, eigvec, selected_dim):
+        """
+        Projects eigenvector onto a subset of 1-2 dimensions for plotting.
+
+        Parameters:
+            1. eigvec: np.ndarray
+                       Eigenvector to be projected.
+            2. selected_dim: Tuple[int]
+                             Tuple of indices of the projected dimensions.
+
+        Returns:
+            1. projected_eigvec: np.ndarray
+                                 [PLACEHOLDER TEXT]
+        """
+        raise NotImplementedError("Need to implement _project_eigvec method.")
+
+    def plot_eigenstate_population(self, idx, selected_dim):
+        """
+        Returns a plot of the population distribution of a specific eigenstate in the
+        projected dimensions.
+
+        Parameters:
+            1. idx: int
+                    Index of the eigenstate to plot.
+            2. selected_dim: Tuple[int]
+                             Tuple of indices of the projected dimensions.
+                             Note: Tuple must have length <= 2 to provide an axis for
+                             population.
+        """
+        if (selected_dim is None and self.hamiltonian.lattice.dimension > 2) or \
+                (len(selected_dim) > 2):
+            raise ValueError("Must project to <= 2 dimensions to plot population "
+                             "distribution.")
+        elif (selected_dim is not None) and (len(selected_dim) == 0):
+            raise ValueError("Must select at least one dimension to plot population "
+                             "distribution.")
+        eigvals, eigvecs = np.linalg.eigh(self.hamiltonian.construct_hamiltonian())
+
+        # Need to reorder eigenvectors such that they are sorted based on
+        # increasing eigenvalue
+        idx_sorted = np.argsort(eigvals)
+        eigvecs = eigvecs[:, idx_sorted]
+        selected_eigvec = eigvecs[:, idx]
+
+        # Project eigenvector onto selected dimensions
+        projected_eigvec = self._project_eigvec(selected_eigvec, selected_dim)
+
+        # Plot population distribution
+        pass
