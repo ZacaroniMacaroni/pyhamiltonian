f330251064e70b6f47a47fd87ec6e7e8b936708d
Zachary W Freeman <z8applepi@gmail.com>
2025-12-08 19:31:57 -0600

    Update tests for plotting enhancements and lattice configuration: - Modify lattice initialization to include disorder parameters. - Add tests for `plot_eigenstate_population` methods in `NDimPlot` and `NParticlePlot`. - Refactor energy spectrum plotting to ensure coverage for new changes.

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,18 @@
 from hamiltonian_construction.hamiltonians import SingleParticleHamiltonian, \
     ManyBodyHamiltonian, Lattice
-from plotting.plotters import NDimPlot
+from plotting.plotters import NDimPlot, NParticlePlot
 
-lat = Lattice(2, 2)
+lat = Lattice(50, 2, disorder_type='uniform', disorder_strength=0.5)
 
 ham = SingleParticleHamiltonian(lat, 1.)
-print(ham.state_list)
 print(ham.construct_hamiltonian())
-
 nd_plot = NDimPlot(ham)
-fig, ax = nd_plot.plot_energy_spectrum()
-fig.show()
+nd_plot.plot_energy_spectrum()
+nd_plot.plot_eigenstate_population(0, (0, 1))
 
-mbham = ManyBodyHamiltonian(lat, 2, 1.)
-print(mbham.state_list)
+lat_1d = Lattice(50, 1, disorder_type='uniform', disorder_strength=0.5)
+mbham = ManyBodyHamiltonian(lat_1d, 2, 1.)
 print(mbham.construct_hamiltonian())
+np_plot = NParticlePlot(mbham)
+np_plot.plot_energy_spectrum()
+np_plot.plot_eigenstate_population(0)
